6f06cf58f688ee1f60d3a4d7b04a7840d5473767
Fatih Ernalbant <fatihernalbant@gmail.com>
2025-08-18 18:01:12 +0300

    Fix typo in hello function return message

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = modal.App("playalter-test")
 
 @app.function()
 def hello():
-    return "PLAYALTER Modal'da çalışıyor!"
+    return "PLAYALTER Modal'da çalişiyor!"
 
 @app.local_entrypoint()
 def main():
